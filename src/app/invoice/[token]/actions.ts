c9dc1bb16c83a2a79b0d93bd297383b97a794ac5
Obafemi Ogunmokun <femzy123@yahoo.com>
2026-07-05 23:01:54 +0100

    update base url fallback

--- src/app/invoice/[token]/actions.ts
+++ src/app/invoice/[token]/actions.ts
@@ -1,6 +1,5 @@
 "use server";
 
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { runtimeEnv } from "@/lib/env";
@@ -24,21 +23,8 @@ function normalizeAppUrl(value: string | null | undefined) {
   return `https://${trimmed}`;
 }
 
-async function getCheckoutAppUrl() {
-  const requestHeaders = await headers();
-  const forwardedHost = requestHeaders.get("x-forwarded-host");
-  const forwardedProto = requestHeaders.get("x-forwarded-proto") ?? "https";
-  const host = forwardedHost ?? requestHeaders.get("host");
-
-  if (host) {
-    return `${forwardedProto}://${host}`.replace(/\/$/, "");
-  }
-
-  return (
-    normalizeAppUrl(requestHeaders.get("origin")) ??
-    normalizeAppUrl(process.env.VERCEL_URL) ??
-    normalizeAppUrl(runtimeEnv.appUrl)
-  );
+function getCheckoutAppUrl() {
+  return normalizeAppUrl(runtimeEnv.appUrl) ?? "http://localhost:3000";
 }
 
 export async function payInvoiceAction(formData: FormData) {
@@ -48,12 +34,7 @@ export async function payInvoiceAction(formData: FormData) {
     redirect("/");
   }
 
-  const appUrl = await getCheckoutAppUrl();
-
-  if (!appUrl) {
-    throw new Error("A public app URL is required to create Checkout orders.");
-  }
-
+  const appUrl = getCheckoutAppUrl();
   const checkoutUrl = await createOrReuseCheckoutForPublicInvoice(token, {
     appUrl,
   });
